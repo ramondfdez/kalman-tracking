4cc29bd5aea24177d0185324362dc2eb311f6000
ramondfdez <dsrmaniaco@gmail.com>
2020-09-19 17:40:30 +0200

    Update kalmanFilter.py

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -1,67 +1,94 @@
-#################### Import Section of the code #############################
-
-try:
-	import numpy as np	
-except Exception as e:
-	print(e,"\nPlease Install the package")
-
-#################### Import Section ends here ################################
+# Import python libraries
+import numpy as np
 
 
 class KalmanFilter(object):
-	"""Kalman Filter class keeps track of the estimated state of
-   	 the system and the variance or uncertainty of the estimate."""
+    """Kalman Filter class keeps track of the estimated state of
+    the system and the variance or uncertainty of the estimate.
+    Predict and Correct methods implement the functionality
+    Reference: https://en.wikipedia.org/wiki/Kalman_filter
+    Attributes: None
+    """
 
-	def __init__(self, dt=1,stateVariance=1,measurementVariance=1, 
-														method="Velocity" ):
-		super(KalmanFilter, self).__init__()
-		self.method = method
-		self.stateVariance = stateVariance
-		self.measurementVariance = measurementVariance
-		self.dt = dt
-		self.initModel()
-	
-	"""init function to initialise the model"""
-	def initModel(self): 
-		if self.method == "Accerelation":
-			self.U = 1
-		else: 
-			self.U = 0
-		self.A = np.matrix( [[1 ,self.dt, 0, 0], [0, 1, 0, 0], 
-										[0, 0, 1, self.dt],  [0, 0, 0, 1]] )
+    def __init__(self):
+        """Initialize variable used by Kalman Filter class
+        Args:
+            None
+        Return:
+            None
+        """
+        self.dt = 0.005  # delta time
 
-		self.B = np.matrix( [[self.dt**2/2], [self.dt], [self.dt**2/2], 
-																[self.dt]] )
-		
-		self.H = np.matrix( [[1,0,0,0], [0,0,1,0]] ) 
-		self.P = np.matrix(self.stateVariance*np.identity(self.A.shape[0]))
-		self.R = np.matrix(self.measurementVariance*np.identity(
-															self.H.shape[0]))
-		
-		self.Q = np.matrix( [[self.dt**4/4 ,self.dt**3/2, 0, 0], 
-							[self.dt**3/2, self.dt**2, 0, 0], 
-							[0, 0, self.dt**4/4 ,self.dt**3/2],
-							[0, 0, self.dt**3/2,self.dt**2]])
-		
-		self.erroCov = self.P
-		self.state = np.matrix([[0],[1],[0],[1]])
+        self.A = np.array([[1, 0], [0, 1]])  # matrix in observation equations
+        self.u = np.zeros((2, 1))  # previous state vector
 
+        # (x,y) tracking object center
+        self.b = np.array([[0], [255]])  # vector of observations
 
-	"""Predict function which predicst next state based on previous state"""
-	def predict(self):
-		self.predictedState = self.A*self.state + self.B*self.U
-		self.predictedErrorCov = self.A*self.erroCov*self.A.T + self.Q
-		temp = np.asarray(self.predictedState)
-		return temp[0], temp[2]
+        self.P = np.diag((3.0, 3.0))  # covariance matrix
+        self.F = np.array([[1.0, self.dt], [0.0, 1.0]])  # state transition mat
 
-	"""Correct function which correct the states based on measurements"""
-	def correct(self, currentMeasurement):
-		self.kalmanGain = self.predictedErrorCov*self.H.T*np.linalg.pinv(
-								self.H*self.predictedErrorCov*self.H.T+self.R)
-		self.state = self.predictedState + self.kalmanGain*(currentMeasurement
-											   - (self.H*self.predictedState))
-		
+        self.Q = np.eye(self.u.shape[0])  # process noise matrix
+        self.R = np.eye(self.b.shape[0])  # observation noise matrix
+        self.lastResult = np.array([[0], [255]])
 
-		self.erroCov = (np.identity(self.P.shape[0]) - 
-								self.kalmanGain*self.H)*self.predictedErrorCov
+    def predict(self):
+        """Predict state vector u and variance of uncertainty P (covariance).
+            where,
+            u: previous state vector
+            P: previous covariance matrix
+            F: state transition matrix
+            Q: process noise matrix
+        Equations:
+            u'_{k|k-1} = Fu'_{k-1|k-1}
+            P_{k|k-1} = FP_{k-1|k-1} F.T + Q
+            where,
+                F.T is F transpose
+        Args:
+            None
+        Return:
+            vector of predicted state estimate
+        """
+        # Predicted state estimate
+        self.u = np.round(np.dot(self.F, self.u))
+        # Predicted estimate covariance
+        self.P = np.dot(self.F, np.dot(self.P, self.F.T)) + self.Q
+        self.lastResult = self.u  # same last predicted result
+        return self.u
 
+    def correct(self, b, flag):
+        """Correct or update state vector u and variance of uncertainty P (covariance).
+        where,
+        u: predicted state vector u
+        A: matrix in observation equations
+        b: vector of observations
+        P: predicted covariance matrix
+        Q: process noise matrix
+        R: observation noise matrix
+        Equations:
+            C = AP_{k|k-1} A.T + R
+            K_{k} = P_{k|k-1} A.T(C.Inv)
+            u'_{k|k} = u'_{k|k-1} + K_{k}(b_{k} - Au'_{k|k-1})
+            P_{k|k} = P_{k|k-1} - K_{k}(CK.T)
+            where,
+                A.T is A transpose
+                C.Inv is C inverse
+        Args:
+            b: vector of observations
+            flag: if "true" prediction result will be updated else detection
+        Return:
+            predicted state vector u
+        """
+
+        if not flag:  # update using prediction
+            self.b = self.lastResult
+        else:  # update using detection
+            self.b = b
+        C = np.dot(self.A, np.dot(self.P, self.A.T)) + self.R
+        K = np.dot(self.P, np.dot(self.A.T, np.linalg.inv(C)))
+
+        self.u = np.round(self.u + np.dot(K, (self.b - np.dot(self.A,
+                                                              self.u))))
+        self.P = self.P - np.dot(K, np.dot(C, K.T))
+        self.lastResult = self.u
+        return self.u
